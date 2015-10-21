c512f1b006d7117d9b6ccd986337c70bea5e4a79
Holman Gao <holman@golmansax.com>
2015-10-21 16:16:32 -0700

    Remove exposure to node_env

--- client/config.from_server.js
+++ client/config.from_server.js
@@ -1,4 +1,3 @@
 /* eslint-disable max-len, no-process-env */
 
-export { NODE_ENV } from '../server/config';
 export const FIREBASE_URL = `https://${process.env.FIREBASE_APP}.firebaseio.com`;
--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-process-env */
 
-export const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || 'development';
 export const isDevelopment = () => NODE_ENV === 'development';
 export const isProduction = () => NODE_ENV === 'production';
 
