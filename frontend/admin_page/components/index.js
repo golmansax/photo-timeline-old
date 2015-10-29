c33df1d668ff8bb379b4006aedb0035796dfcfd8
Holman Gao <holman@golmansax.com>
2015-10-29 00:04:04 -0700

    Forgot a file

--- frontend/admin_page/components/index.js
+++ frontend/admin_page/components/index.js
@@ -1,12 +1,10 @@
 import AdminPageContent from './content';
-import AdminLogin from './login';
 import AdminEditEvent from './edit_event';
 import AdminEventList from './event_list';
 import AdminNewEvent from './new_event';
 
 export {
   AdminPageContent,
-  AdminLogin,
   AdminEventList,
   AdminEditEvent,
   AdminNewEvent,
