9dc3b8ccf54f4b3d4a5540a0632e766fde8adf97
Holman Gao <holman@golmansax.com>
2016-02-02 09:00:17 -0800

    Fix lints

--- frontend/events/actions.js
+++ frontend/events/actions.js
@@ -1,25 +1,21 @@
 import { childRef } from '_client/firebase_ref';
 
-export const saveEvent = (id, data) => {
-  return new Promise((resolve, reject) => {
-    childRef(`events/${id}`).set(data, (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
+export const saveEvent = (id, data) => new Promise((resolve, reject) => {
+  childRef(`events/${id}`).set(data, (error) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve();
+    }
   });
-};
+});
 
-export const removeEvent = (id) => {
-  return new Promise((resolve, reject) => {
-    childRef(`events/${id}`).remove((error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
+export const removeEvent = (id) => new Promise((resolve, reject) => {
+  childRef(`events/${id}`).remove((error) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve();
+    }
   });
-};
+});
