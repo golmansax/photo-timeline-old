c062db34a8819a98e106c4c74bc4388a57f62668
Holman Gao <holman@golmansax.com>
2016-06-02 21:00:26 -0400

    Incorporate photoswipe into grid

--- frontend/events/components/grid.js
+++ frontend/events/components/grid.js
@@ -1,26 +1,59 @@
-import { PropTypes } from 'react';
+import { Component, PropTypes } from 'react';
+import { PhotoSwipe } from 'react-photoswipe';
 import { A } from '_frontend/components';
 import moment from 'moment';
 import EventImage from './image';
 import styles from './grid.css';
 
-const renderEvent = (event) => (
-  <div className={styles.gridItem} key={event.key}>
-    <div><A route={`/events/${event.key}`}>{event.title}</A></div>
+const EventGridItem = ({ event, onClick }) => (
+  <div className={styles.gridItem}>
+    <div><A href='#' onClick={onClick}>{event.title}</A></div>
     <div>{moment(event.date).format('MMMM Do YYYY')}</div>
     <div>{event.location}</div>
-    <EventImage event={event} className={styles.image} />
+    <A href='#' onClick={onClick}>
+      <EventImage event={event} className={styles.image} />
+    </A>
   </div>
 );
 
-const EventGrid = ({ events }) => (
-  <div className={styles.grid}>
-    {events.map(renderEvent)}
-  </div>
-);
+class EventGrid extends Component {
+  static propTypes = {
+    events: PropTypes.array.isRequired,
+  };
 
-EventGrid.propTypes = {
-  events: PropTypes.array.isRequired,
-};
+  state = {
+    photoswipeOpen: false,
+    selectedIndex: 0,
+  };
+
+  render = () => (
+    <div>
+      <div className={styles.grid}>
+        {this.props.events.map((event, index) => (
+          <EventGridItem
+            key={event.key}
+            event={event}
+            onClick={this._openPhotoswipe.bind(this, index)}
+          />
+        ))}
+      </div>
+      <PhotoSwipe
+        isOpen={this.state.photoswipeOpen}
+        items={this.props.events.map((event) => ({
+          src: event.imageUrl,
+          title: event.title,
+          w: 1200,
+          h: 900,
+        }))}
+        options={{ index: this.state.selectedIndex }}
+      />
+    </div>
+  );
+
+  _openPhotoswipe = (selectedIndex, event) => {
+    event.preventDefault();
+    this.setState({ photoswipeOpen: true, selectedIndex });
+  };
+}
 
 export default EventGrid;
--- frontend/home_page/components/main_display.js
+++ frontend/home_page/components/main_display.js
@@ -1,12 +1,10 @@
 import { Component } from 'react';
-import { PhotoSwipe } from 'react-photoswipe';
 import { bindToState, removeBinding } from '_client/re_base';
 import { EventGrid } from '../../events/components';
 
 class HomeMainDisplay extends Component {
   state = {
     events: null,
-    isOpen: false,
   };
 
   componentWillMount() {
@@ -25,26 +23,8 @@ class HomeMainDisplay extends Component {
   render() {
     if (this.state.events === null) { return <div>Loading...</div>; }
 
-    const images = this.state.events.map((event) => ({
-      src: event.imageUrl,
-      title: event.title,
-      w: 1200,
-      h: 900,
-    }));
-    return (
-      <div>
-        <EventGrid events={this.state.events} />
-        <button onClick={this._onOpen}>Open</button>
-        <PhotoSwipe
-          isOpen={this.state.isOpen}
-          items={images}
-        />
-      </div>
-    );
+    return <EventGrid events={this.state.events} />;
   }
-
-  _onOpen = () => this.setState({ isOpen: true });
-  _onClose = () => this.setState({ isOpen: false });
 }
 
 export default HomeMainDisplay;
